ec82353e1c023741ff389650f9ccb448fc7dc25b
thweasel <thweasel@gmail.com>
2018-12-16 19:07:36 +0000

    Working example, can load some sector or uncomment and load a record

--- src/main.c
+++ src/main.c
@@ -3,26 +3,63 @@
 #include <spectrum.h>
 #include <zxinterface1.h>
 #include <malloc.h>
+#include <strings.h>
+#include <input.h>
 
-char buffer[sizeof(struct M_CHAN)];
-char buffer[50] = "10 border 4";
-int f;
-
-long heap;
-
-FILE *fp;
-
+static struct M_CHAN MD;
+static struct M_MAP MD_MAP;
+static struct M_CHAN *pMD;
+static u8_t *p;
+static  int result;
+u8_t byte;
 main()
 {
-        mallinit();
+        int drvN = 1;
+        pMD=&MD;
+        clg();
+        if1_update_map(2,MD_MAP);
+        printf("Microdrive status = %d \n",if1_mdv_status(drvN));
+        printf("Free sectors = %d \n",if1_free_sectors(drvN));
 
+        for(int i=70;i<76;i++)
+        {
+                result = if1_load_sector(drvN,i,MD);
+                //result = if1_load_record(drvN,"file.bas",0,MD);
+                printf("\nresult (sector) = %u",result);
+                if (result!=-1);
+                {       
+                        printf("\nDrive: %u ",MD->drive);
+                        printf("\nsector: %u ",MD->sector);
+                        printf("\nhdname: %s ",if1_getname(MD->hdname));
+                        printf("\nname:  %s ",if1_getname(MD->name));
+                        printf("\nrecname:  %s ",if1_getname(MD->recname));
+                                               
+                        
+                        printf("\nbc: %u ",MD->bytecount);
+                        printf("\nrecnum: %u reclen: %u recflg: %u",MD->recnum,MD->reclen,MD->recflg);
+                        printf("\nhdpreamble: %u",MD->hdpreamble[0]);
 
-        if1_init_file(1,"testfile",buffer);
-        fp = fopen("testfile","w");
+                        printf("\nDATA\n");
+                        for (int i=0;i<100;i++)
+                        {
+                                //printf("%u:",MD.data[i]);
+                                byte=MD->data[i];
+                                printf("%X:",byte);
+                        }
+                        
+                        printf("\nMAP:\n");
+                        for (int i=0;i<32;i++)
+                        {
+                                //printf("%d:",MD_MAP.map[i]);
+                                byte=MD_MAP->map[i];
+                                printf("%X:",byte);
 
-        for (f=0; f<sizeof(buffer); f++)
-        {                
-                fputs(buffer[f], fp);
+                        }
+                }
+                
+                printf("\nEND\n\n");
+                //in_Wait(10);
+                //clg();
         }
-        fclose(fp);
+        
 }
